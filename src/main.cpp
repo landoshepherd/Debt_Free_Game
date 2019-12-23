7ce9de760edd0eb1a54151666db0b98ce4be7820
Lando Shepherd <landolshepherd@gmail.com>
2019-12-23 11:33:27 -0500

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Debt_Free_Game
+//
+//  Created by Lando Shepherd on 12/23/19.
+//  Copyright © 2019 Lando Shepherd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
